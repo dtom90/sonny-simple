fa557b347c2ef6790d9e8faccfddaa7edf793c0b
David Thomason <dlthomas@us.ibm.com>
2016-08-05 08:55:33 -0400

    Added Workspace file

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ var conversation = watson.conversation({
 });
 
 // App-specific settings
-var DEBUG = false;  // Debug the app (excluding the Weather Underground Util)
+var DEBUG = process.env.DEBUG == 'true' || false;  // Debug the app (excluding the Weather Underground Util)
 var weatherUtil = require('./lib/weatherUtil'); // Require the Weather Underground Util
 var WU_API_KEY = process.env.WEATHER_UNDERGROUND_API_KEY || null; // Get Weather Underground API key from the .env file
-var DEBUG_UTIL = false; // Debug the Weather Underground Util
+var DEBUG_UTIL = true; // Debug the Weather Underground Util
 
 // Endpoint to be called from the client side
 app.post('/api/message', function(req, res) {
@@ -93,7 +93,7 @@ app.post('/api/message', function(req, res) {
         res.json(data); // respond with the JSON data
       });
 
-    else{                                                               // if we are not yet ready to get the weather, (i.e. still in the dialog)
+    else {                                                              // if we are not yet ready to get the weather, (i.e. still in the dialog)
       data.output.text = data.output.text[data.output.text.length - 1];   // then set the output to be the last element in the array of output text strings
       res.json(data);                                                     // and respond with the JSON data
     }
@@ -106,12 +106,38 @@ app.post('/api/message', function(req, res) {
  * @param {Function} callback: to be called when we have the response from the weather util
  */
 function makeWeatherRequest(data, callback){
-	weatherUtil.makeWeatherRequest(WU_API_KEY, data.context.condition, data.context.city, data.context.state, data.context.date, DEBUG_UTIL, 
+  
+  if(data.context.city instanceof Array)
+    data.context.city = data.context.city[data.context.city.length-1].value;  // set the city to be the value of the last element in the array
+  
+  if (data.context.state && data.context.state instanceof Array){
+    var state = null;
+    console.log(data.context.state);
+    var state_city_mismatches = data.context.state.filter(function (x){ return x.value != data.context.city });  // get the number of states that do not match the city
+    console.log(state_city_mismatches);
+    if(state_city_mismatches.length > 0)
+      state = state_city_mismatches[state_city_mismatches.length-1].value;
+    else {
+      var state_city_matches = data.context.state.filter(function (x){ return x.value == data.context.city });  // get the number of states that match the city
+      if(state_city_matches.length >= 2)
+        state = state_city_matches[state_city_matches.length-1]; // set the state to be the value of the last element in the array
+    }
+    data.context.state = state;
+  }
 
-  function(weather_reply){                      // callback function called from the weatherUtil
+  // set the last line of the output text ("<Condition> in ") to include the updated city and state
+  data.output.text[data.output.text.length - 1] += data.context.city;
+  
+  // make the weather request
+  if(DEBUG) console.log("Making weather request for " + data.context.condition + "," + data.context.city + "," + data.context.state + "," + data.context.date);
+	weatherUtil.makeWeatherRequest(WU_API_KEY, data.context.condition, data.context.city, data.context.state, data.context.date, DEBUG_UTIL, 
+  function(weather_reply, state){                      // callback function called from the weatherUtil
+    if(state) data.output.text[data.output.text.length - 1] += ", " + state;
+    data.output.text[data.output.text.length - 1] += ":";
+
     if(weather_reply.ask) {                       // if we need to ask the user a question
       data.context.asked_state = true;              // set the asked_state context varaible to true
-      data.context.options = weather_reply.options; // set the options of what to ask (TODO: use in Watson Conversation Dialog)
+      // data.context.options = weather_reply.options; // set the options of what to ask (TODO: use in Watson Conversation Dialog)
       callback(weather_reply.ask);                  // call the callback function with the output text (weather_reply.ask)
     } else                                        // if don't need to ask a question,
       callback(weather_reply.tell);                 // call the callback function with the output text (weather_reply.tell)
